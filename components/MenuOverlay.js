81b8ea028573b343d621fc367636ae811021042f
Matteo Calderaro <matteo.calderaro@binetwork.it>
2025-12-10 08:37:59 +0100

    sistemazione stili menu overlay

--- components/MenuOverlay.js
+++ components/MenuOverlay.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import applications from '../data/applications';
 
@@ -8,14 +7,21 @@ const MenuOverlay = ({ closeMenu, isMenuOpen }) => { // Riceve isMenuOpen
 
   return (
     <div className={`menu-overlay ${isMenuOpen ? 'is-open' : ''}`}> {/* Applica classe is-open */}
-      <ul className="list-unstyled text-center w-100">
+      <h6>APPLICAZIONI</h6>
+      <ul className="list-unstyled w-100">
         {applications
           .filter(app => !app.isHome)
           .map((app) => (
-            <li key={app.path} className={`${router.asPath === `/${app.path}` ? 'active' : ''}`}>
-              <Link href={`/${app.path}`} onClick={closeMenu}>
-                {app.heroContent.brand}
-              </Link>
+            <li
+              key={app.path}
+              className={`${router.asPath === `/${app.path}` ? 'active' : ''}`}
+              onClick={() => {
+                closeMenu();
+                router.push(`/${app.path}`);
+              }}
+              style={{ cursor: 'pointer' }} // Aggiungiamo uno stile per indicare che è cliccabile
+            >
+              {app.heroContent.brand}
             </li>
           ))}
       </ul>
